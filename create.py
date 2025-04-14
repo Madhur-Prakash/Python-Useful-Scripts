3f08167fc7e555b2170a65c8335412eab25df18f
Madhur <madhurprakash2005@gmail.com>
2025-04-14 20:39:45 +0530

    Add virtual environment name to .gitignore upon creation

--- create.py
+++ create.py
@@ -9,6 +9,22 @@ def create_venv():
         print("Creating virtual environment...")
         os.system(f'python -m venv "{venv_name}"')
         print("Virtual environment created successfully.")
+
+        if not os.path.exists(".gitignore"):
+            open(".gitignore", "w").close()
+        with open(".gitignore", "rb+") as f:
+            f.seek(0, os.SEEK_END)
+            if f.tell() > 0:
+                f.seek(-1, os.SEEK_END)
+                last_char = f.read(1)
+                if last_char != b'\n':
+                    f.write(b'\n')
+
+    # Now append the venv name
+        with open(".gitignore", "a", encoding="utf-8") as f:
+            f.write(f"{venv_name}\n")
+
+        print(f"Added '{venv_name}' to .gitignore")
         res = (f"./{venv_name}/Scripts/activate\n")
         print("To activate the virtual environment type:\n")
         print(res)
